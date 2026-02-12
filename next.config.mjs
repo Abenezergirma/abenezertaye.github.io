a71f2f51aa15d51106db144dcbbe96f34a895a4a
Abenezergirma <abengirma1@gmail.com>
2026-02-12 13:38:23 -0600

    Add base path for project pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    basePath: process.env.NEXT_PUBLIC_BUILD_STANDALONE === 'true' ? '/abenezertaye.github.io' : '',
+    assetPrefix: process.env.NEXT_PUBLIC_BUILD_STANDALONE === 'true' ? '/abenezertaye.github.io/' : '',
 };
 
 export default nextConfig;
